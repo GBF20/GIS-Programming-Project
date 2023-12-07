d257552dcf13bdad09a1f3c8e511d19dfc3d4a62
GBF20 <143640121+GBF20@users.noreply.github.com>
2023-12-07 13:48:16 -0600

    Radiometric Calibration
    First run repository clone file followed by the radiometric calibration file. Both can be done by using a miniconda prompt.

---
+++ Radiometric Calibration.py
@@ -0,0 +1,4 @@
+cd py-radiometric-corrections/
+conda activate imgcorrect-venv
+python scripts\correct_images.py -h
+python scripts\correct_images.py “” --output “”
---
+++ Repository Clone.py
@@ -0,0 +1,5 @@
+git clone https://github.com/SenteraLLC/py-radiometric-corrections.git
+cd py-radiometric-corrections/
+conda env create -f environment.yml
+conda activate imgcorrect-venv
+Pip install . 
